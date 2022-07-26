f4c29d8e2c5895d04e88ad13c26aae6579e4531b
KazutoshiAsakawa <k020g1206@g.neec.ac.jp>
2022-07-26 13:51:02 +0900

    単色シェーダー

--- solution/Resources/shaders/FBXPS.hlsl
+++ solution/Resources/shaders/FBXPS.hlsl
@@ -9,20 +9,25 @@ struct PSOutput {
 	float4 target1 : SV_TARGET1;
 };
 
-PSOutput main(VSOutput input) //: SV_TARGET
+//PSOutput main(VSOutput input) //: SV_TARGET
+//{
+//	PSOutput output;
+//
+//	// テクスチャマッピング
+//	float4 texcolor = tex.Sample(smp, input.uv);
+//	// Lambert反射
+//	float3 light = normalize(float3(1,-1,1)); // 右下奥　向きのライト
+//	float diffuse = saturate(dot(-light, input.normal));
+//	float brightness = diffuse + 0.3f;
+//	float4 shadecolor = float4(brightness, brightness, brightness, 1.0f);
+//	// 陰影とテクスチャの色を合成
+//	output.target0 = shadecolor * texcolor;
+//	output.target1 = float4(1 - (shadecolor * texcolor).rgb, 1);
+//
+//	return output;
+//}
+
+float4 main() : SV_TARGET
 {
-	PSOutput output;
-
-	// テクスチャマッピング
-	float4 texcolor = tex.Sample(smp, input.uv);
-	// Lambert反射
-	float3 light = normalize(float3(1,-1,1)); // 右下奥　向きのライト
-	float diffuse = saturate(dot(-light, input.normal));
-	float brightness = diffuse + 0.3f;
-	float4 shadecolor = float4(brightness, brightness, brightness, 1.0f);
-	// 陰影とテクスチャの色を合成
-	output.target0 = shadecolor * texcolor;
-	output.target1 = float4(1 - (shadecolor * texcolor).rgb, 1);
-
-	return output;
+	return float4(0.0f, 1.0f, 1.0f, 1.0f);
 }
--- solution/Resources/shaders/PostEffectTestPS.hlsl
+++ solution/Resources/shaders/PostEffectTestPS.hlsl
@@ -4,7 +4,7 @@ Texture2D<float4> tex0 : register(t0);  // 0番スロットに設定されたテ
 Texture2D<float4> tex1 : register(t1);  // 1番スロットに設定されたテクスチャ
 SamplerState smp : register(s0);      // 0番スロットに設定されたサンプラー
 
-float vignette(float2 uv){
+float vignette(float2 uv) {
 	// 中心からの距離
 	float2 dista2 = distance(uv, 0.5f);
 	// 距離が遠いほど暗くする
@@ -15,33 +15,33 @@ float vignette(float2 uv){
 float Gaussian(float2 drawUV, float2 pickUV, float sigma)
 {
 	// 描画座標と取得座標の距離
-    float d = distance(drawUV, pickUV);
-    return exp(-(d * d) / (2 * sigma * sigma));
+	float d = distance(drawUV, pickUV);
+	return exp(-(d * d) / (2 * sigma * sigma));
 }
 
 float4 GaussianBlur(Texture2D<float4> tex, float2 uv, float sigma = 0.005, float stepWidth = 0.001)
 {
-    float _ShiftWidth = 0.005f;
-    float _ShiftNum = 3;
+	float _ShiftWidth = 0.005f;
+	float _ShiftNum = 3;
 
 	float totalWeight = 0;
-    float4 col = float4(0, 0, 0, 1);
+	float4 col = float4(0, 0, 0, 1);
 
-    for (float py = -sigma * 2; py <= sigma * 2; py += stepWidth)
-    {
-        for (float px = -sigma * 2; px <= sigma * 2; px += stepWidth)
-        {
-            float2 pickUV = uv + float2(px, py);
-            float weight = Gaussian(uv, pickUV, sigma);
-            col += tex.Sample(smp, pickUV) * weight;
-            totalWeight += weight;
-        }
-    }
-    col.rgb = col.rgb / totalWeight;
-    return col;
+	for (float py = -sigma * 2; py <= sigma * 2; py += stepWidth)
+	{
+		for (float px = -sigma * 2; px <= sigma * 2; px += stepWidth)
+		{
+			float2 pickUV = uv + float2(px, py);
+			float weight = Gaussian(uv, pickUV, sigma);
+			col += tex0.Sample(smp, pickUV) * weight;
+			totalWeight += weight;
+		}
+	}
+	col.rgb = col.rgb / totalWeight;
+	return col;
 }
 
-float4 RgbShift(Texture2D<float4> tex, float2 uv, float2 shiftR , float2 shiftG , float2 shiftB){
+float4 RgbShift(Texture2D<float4> tex, float2 uv, float2 shiftR, float2 shiftG, float2 shiftB) {
 	float4 retCol;
 	retCol.r = tex.Sample(smp, uv + shiftR).r;
 	retCol.g = tex.Sample(smp, uv + shiftG).g;
@@ -51,7 +51,7 @@ float4 RgbShift(Texture2D<float4> tex, float2 uv, float2 shiftR , float2 shiftG 
 	return retCol;
 }
 
-float4 RgbShiftGaussianBlur(Texture2D<float4> tex, float2 uv, float2 shiftR , float2 shiftG , float2 shiftB){
+float4 RgbShiftGaussianBlur(Texture2D<float4> tex, float2 uv, float2 shiftR, float2 shiftG, float2 shiftB) {
 	float4 retCol;
 	retCol.r = GaussianBlur(tex, uv + shiftR).r;
 	retCol.g = GaussianBlur(tex, uv + shiftG).g;
@@ -69,22 +69,28 @@ float4 main(VSOutput input) : SV_TARGET
 	// RgbShift
 	float4 rgbShiftColor = RgbShiftGaussianBlur(tex0,
 												mosaicUv,
-												float2(0.000f, 0.0000f),
-												float2(0.005f, 0.0000f),
+												float2(0.000f, 0.0020f),
+												float2(0.010f, 0.0000f),
 												float2(0.000f, 0.0025f));
 
 	float4 retColor = rgbShiftColor;
-	float4 colortex1 = tex1.Sample(smp, mosaicUv);
+	float4 colortex1 = tex0.Sample(smp, mosaicUv);
+
 	if (fmod(mosaicUv.y, 0.1f) < 0.05f) {
 		retColor = colortex1;
+		return float4(1.0f - retColor.rgb, 1);
 	}
 
 	// vignatte
 	float vignVal = vignette(mosaicUv);
 
 	// Gaussian
-	//float4 blurColor = GaussianBlur(tex0, mosaicUv);
+	float4 blurColor = GaussianBlur(tex1, mosaicUv);
 
 
 	return float4(retColor.rgb - vignVal, 1.0f);
+
+
 }
+
+// return float4(1.0f - color.rgb,1);
